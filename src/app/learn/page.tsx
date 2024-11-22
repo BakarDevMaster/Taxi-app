b6e25f75c9b185a5f875afb3312114afeb4334a3
BakarDevMaster <143206945+BakarDevMaster@users.noreply.github.com>
2024-11-22 15:15:09 +0500

    removed framer motion

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import LearnHero from '../Components/LearnComponents/LearnHero/LearnHero'
-import { div } from 'framer-motion/client'
 import LearnMaterials from '../Components/LearnComponents/LearnMaterials/LearnMaterials'
 
 const page = () => {
